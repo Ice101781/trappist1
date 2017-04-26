a3a5fe90f337a8b823c35704459e66a64e8507cb
Joe C. Antonelli <joecantonelli@gmail.com>
2017-04-26 02:06:20 -0400

    Implement constructor/prototype and dynamic prototype patterns to improve OO consistency

--- js/index.js
+++ js/index.js
@@ -13,77 +13,54 @@
 var starSystem;
 
 window.onload = function() {
+    // add a method to convert from rectangular to spherical coordinates
+    Math.sphericalToVec3 = function(r, theta, phi) {
+        var x = r*Math.cos(theta)*Math.sin(phi),
+            y = r*Math.sin(theta)*Math.sin(phi),
+            z = r*Math.cos(phi);
+        return new THREE.Vector3(x,y,z);
+    }
 
-    var Calc = function() {
-        // azimuthal angle = theta, polar angle = phi, r >= 0, 0 <= theta <= PI, 0 <= phi < 2*PI
-        function fromSpherical(r, theta, phi) {
-            var x = r*Math.cos(theta)*Math.sin(phi),
-                y = r*Math.sin(theta)*Math.sin(phi),
-                z = r*Math.cos(phi);
+    // renderer
+    function Renderer(attr) {
+        THREE.WebGLRenderer.call(this, attr);
+    }
+    (function(superProto) {
+        Renderer.prototype = Object.create(superProto);
+        Renderer.prototype.constructor = Renderer;
 
-            return new THREE.Vector3(x,y,z);
+        Renderer.prototype.setup = function(col, opac, par) {
+            this.setClearColor(col, opac);
+            this.setSize(par.offsetWidth, par.offsetHeight);
+            par.appendChild(this.domElement);
         }
-        return {
-            fromSpherical: fromSpherical
-        };
-    }();
+    })(THREE.WebGLRenderer.prototype);
 
-    var Renderer = function() {
-        function setup(attr, col, opac, par) {
-            var ren = new THREE.WebGLRenderer(attr);
-            ren.setClearColor(col, opac);
-            ren.setSize(par.offsetWidth, par.offsetHeight);
-            par.appendChild(ren.domElement);
-            return ren;
+
+    // camera
+    function Camera(vfovDeg, asp, near) {
+        THREE.PerspectiveCamera.call(this, vfovDeg, asp, near, near*Math.pow(10,5));
+    }
+    (function(superProto) {
+        Camera.prototype = Object.create(superProto);
+        Camera.prototype.constructor = Camera;
+
+        Camera.prototype.orbit = function() {
+            // TO DO
         }
-        return {
-            setup: setup
-        };
-    }();
-
-    var Light = function() {
-        function setup(typ, col, pos) {
-            var lgt,
-                pos = typeof(pos) !== 'undefined' ? pos : new THREE.Vector3();
-
-            switch(typ) {
-                case "Ambient":
-                    lgt = new THREE.AmbientLight(col);
-                    break;
-                case "Point":
-                    lgt = new THREE.PointLight(col, 1, 0, 2);
-                    lgt.position.copy(pos);
-                    break;
-            }
-            return lgt;
+        Camera.prototype.repel = function(tgt, spd) {
+            this.position.z += spd;
+            this.lookAt(tgt);
         }
-        return {
-            setup: setup
-        };
-    }();
+    })(THREE.PerspectiveCamera.prototype);
 
-    var Camera = function() {
-        function setup(vfovDeg, aspect, near, pos) {
-            var cam = new THREE.PerspectiveCamera(vfovDeg, aspect, near, near*Math.pow(10,5));
-            cam.position.copy(pos);
-            return cam;
-        }
-        function path(cam, speed, tgt) {
-            cam.position.z += speed; // write orbit code here...
-            cam.lookAt(tgt);
-        }
-        return {
-            setup: setup,
-            path: path
-        };
-    }();
 
-    var Satellite = function() {
-        // texture mapping
+    // star, planet, moon, etc.
+    function Satellite(rad, numSegs, matTyp, imgLoc) {
+        // map texture from image to material
         function texToMat(matTyp, imgLoc) {
             var img = new Image(),
                 mat = matTyp === "Phong" ? new THREE.MeshPhongMaterial() : new THREE.MeshBasicMaterial();
-
             img.src = imgLoc;
             img.onload = function() {
                 var tex = new THREE.Texture(this);
@@ -96,78 +73,60 @@ window.onload = function() {
             }
             return mat;
         }
-        function create(rad, numSegs, matTyp, imgLoc, parPos, relPos) {
-            var sat = new THREE.Mesh(new THREE.SphereGeometry(rad, numSegs, numSegs), texToMat(matTyp, imgLoc)),
-                parPos = typeof(parPos) !== 'undefined' ? parPos : new THREE.Vector3(),
-                relPos = typeof(relPos) !== 'undefined' ? relPos : new THREE.Vector3();
 
-            sat.position.copy(parPos.clone().add(relPos));
-            return sat;
-        }
-        return {
-            create: create
-        };
-    }();
+        THREE.Mesh.call(this, new THREE.SphereGeometry(rad, numSegs, numSegs), texToMat(matTyp, imgLoc));
+    }
+    (function(superProto) {
+        Satellite.prototype = Object.create(superProto);
+        Satellite.prototype.constructor = Satellite;
+    })(THREE.Mesh.prototype);
 
-    starSystem = function(MATH_MOD, RENDERER_MOD, LIGHT_MOD, CAMERA_MOD, SATELLITE_MOD) {
+
+    function spaceScene() {
         var container = document.getElementById("threejs-container"),
-            renderer = Renderer.setup({antialias: false}, 'black', 1, container),
+            renderer = new Renderer({antialias: false}),
             universe = new THREE.Scene(),
-            ambLight = Light.setup("Ambient", "rgb(66,93,120)"),
-            ptLight = Light.setup("Point", "rgb(255,185,120)"),
-            shipCam = Camera.setup(45, container.offsetWidth/container.offsetHeight, 0.05, Calc.fromSpherical(2700,0,0).add(Calc.fromSpherical(45, 0.25, 0.8))),
-            star = Satellite.create(100, 20, "", "../img/255_185_120_(M8V).jpg"),
-            planet = Satellite.create(12.5, 100, "Phong", "../img/mercury_enhanced_color.jpg", star.position, Calc.fromSpherical(2700,0,0)),
-            moon = Satellite.create(3, 100, "Phong", "../img/ganymede.jpg", planet.position, Calc.fromSpherical(200,0,4*Math.PI/5));
+            ambLight = new THREE.AmbientLight("rgb(66,93,120)"),
+            ptLight = new THREE.PointLight("rgb(255,185,120)"),
+            shipCam = new Camera(45, container.offsetWidth/container.offsetHeight, 0.05),
+            star = new Satellite(100, 20, "", "../img/255_185_120_(M8V).jpg"),
+            planet = new Satellite(12.5, 100, "Phong", "../img/mercury_enhanced_color.jpg"),
+            moon = new Satellite(3, 100, "Phong", "../img/ganymede.jpg"),
+            animate,
+            PLAYING;
 
+        renderer.setup("rgb(0,0,0)", 1, container);
+        star.position.copy(new THREE.Vector3());
+        planet.position.copy(star.position.clone().add(Math.sphericalToVec3(2700,0,0)));
+        moon.position.copy(planet.position.clone().add(Math.sphericalToVec3(200,0,4*Math.PI/5)));
+        shipCam.position.copy(planet.position.clone().add(Math.sphericalToVec3(45, 0.25, 0.8)));
         universe.add(ambLight, ptLight, shipCam, star, planet, moon);
 
-        var Animation = function() {
-            var animate,
-                PLAYING;
-
+        function render() {
             // update scene objects
-            function update() {
-                Camera.path(shipCam, 0.01, Calc.fromSpherical(2400,0,0));
-            }
-            // render and animate the scene
-            function render() {
-                update();
-                renderer.render(universe, shipCam);
-                animate = requestAnimationFrame(render);
-                PLAYING = true;
-            }
-            function start() {
+            shipCam.repel(Math.sphericalToVec3(2400,0,0), 0.1);
+
+            renderer.render(universe, shipCam);
+            animate = requestAnimationFrame(render);
+        }
+
+        // instance API
+        if(typeof(this.toggleAnimation) !== "function") {
+            spaceScene.prototype.toggleAnimation = function() {
                 switch(true) {
                     case !PLAYING:
                         render();
-                        return true;
-                    default:
-                        console.log("The animation has already been started.");
-                        return false;
-                }
-            }
-            function stop() {
-                switch(true) {
+                        PLAYING = true;
+                        return console.log("Animation started.");
                     case PLAYING:
                         cancelAnimationFrame(animate);
                         PLAYING = false;
-                        return true;
-                    default:
-                        console.log("The animation has already been stopped.");
-                        return false;
+                        return console.log("Animation stopped.");
                 }
             }
-            // API
-            return {
-                start: start,
-                stop: stop
-            };
-        }();
-        return {
-            Animation: Animation
-        };
-    }(Calc, Renderer, Light, Camera, Satellite);
+        }
+    }
 
-    starSystem.Animation.start();
+    starSystem = new spaceScene();
+    starSystem.toggleAnimation();
 }
